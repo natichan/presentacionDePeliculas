0ec53ec46478d29e27aa5ccd16425bdf2ad6a26d
Natalia <nataliamh.88@gmail.com>
2018-10-08 12:17:22 -0300

    estilos

--- JS/view/view.js
+++ JS/view/view.js
@@ -3,8 +3,7 @@ const container = document.getElementById('movies')
 renderInfo = (element) => {
         // console.log(element.imdbID);
         return container.innerHTML += 
-        `<div class="accordion col-10" id="accordionExample">
-             <div class="card">
+        `<div class="accordion col-12" id="accordionExample">
                  <div class="card-header" id="x${element.imdbID}" >
                  <h5 class="mb-0">
                      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#${element.imdbID}"  aria-controls="${element.imdbID}">
@@ -12,16 +11,16 @@ renderInfo = (element) => {
                          ${element.Title}     
                      </button>
                  </h5>
-                 </div>
              </div>
          </div>
         <div id=${element.imdbID} class="collapse" aria-labelledby="x${element.imdbID}" data-parent="#accordionExample">
                  <div class="card-body">
-                     <img src="${element.Poster}">
+                     <img src="${element.Poster}" class="img-thumbnail">
                      <p>Title: ${element.Title}</p> 
                      <p>Year: ${element.Year}</p> 
                  </div>
              </div>
+         </div>
          `
     }
 
