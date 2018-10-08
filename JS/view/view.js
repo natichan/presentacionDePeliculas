4edbd44fae0c0e884b58ec3c22aea515ad036539
Natalia <nataliamh.88@gmail.com>
2018-10-08 11:07:03 -0300

    funcionando collapsable por cada una

--- JS/view/view.js
+++ JS/view/view.js
@@ -1,19 +1,21 @@
 const container = document.getElementById('movies')
 
 renderInfo = (element) => {
-        // console.log(element.Title);
+        // console.log(element.imdbID);
         return container.innerHTML += 
         `<div class="accordion col-10" id="accordionExample">
              <div class="card">
-                 <div class="card-header" id="headingOne">
+                 <div class="card-header" id="x${element.imdbID}" >
                  <h5 class="mb-0">
-                     <button class="btn btn-link" type="button" data-toggle="collapse" data-target="${element.imdbID}"  aria-controls="${element.imdbID}">
-                         <img src="${element.Poster}" class="rounded-circle firstTitle">${element.Title}     </button>
+                     <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#${element.imdbID}"  aria-controls="${element.imdbID}">
+                         <img src="${element.Poster}" class="rounded-circle firstTitle">
+                         ${element.Title}     
+                     </button>
                  </h5>
                  </div>
              </div>
          </div>
-                 <div id=${element.imdbID} class="collapse" aria-labelledby="${element.imdbID}" data-parent="#accordionExample">
+        <div id=${element.imdbID} class="collapse" aria-labelledby="x${element.imdbID}" data-parent="#accordionExample">
                  <div class="card-body">
                      <img src="${element.Poster}">
                      <p>Title: ${element.Title}</p> 
