52a825f856320a76631782486d5f4e4203cef14e
Natalia <nataliamh.88@gmail.com>
2018-10-08 16:53:34 -0300

    haciendo tests

--- src/JS/model/model.js
+++ src/JS/model/model.js
@@ -1,11 +1,9 @@
 
-const getDataMovie = () => {
-  return new Promise((resolve, reject) => {
-    fetch('https://www.omdbapi.com/?apikey=348097f5&s=code&type=movie')
-      .then(response => response.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
-  });
-};
+const getDataMovie = () => new Promise((resolve, reject) => {
+  fetch('https://www.omdbapi.com/?apikey=348097f5&s=code&type=movie')
+    .then(response => response.json())
+    .then(data => resolve(data))
+    .catch(err => reject(err));
+});
 
 getDataMovie();
