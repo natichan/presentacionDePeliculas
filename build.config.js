c3697d2517a908fced97766459cf298f17a54d07
Natalia <nataliamh.88@gmail.com>
2018-10-09 11:41:45 -0300

    no usare babel

--- build.config.js
+++
@@ -1,7 +0,0 @@
-paths: {
-    es6: "node_modules/requirejs-babel/es6",
-    babel: "node_modules/requirejs-babel/babel-4.6.6.min"
-}
-define(["es6!your-es6-module"], function (module) {
-    // ...
-  });
