3a96e083e80640f891f07ed9addca87b6105f4f7
Natalia <nataliamh.88@gmail.com>
2018-10-09 10:02:48 -0300

    nuevas reglas para eslint

--- src/Test/model.spec.js
+++ src/Test/model.spec.js
@@ -2,7 +2,7 @@ const model = require('../JS/model/model');
 global.fetch = require('node-fetch');
 
 describe('function getDataMovie()', () => {
-    test('obtiene la respuesta de una promesa fetch', () => {
-      return expect(model.getDataMovie('https://www.omdbapi.com/?apikey=348097f5&s=code&type=movie')).resolves.toBeTruthy();
-    });
+  test('obtiene la respuesta de una promesa fetch', () => {
+    return expect(model.getDataMovie('https://www.omdbapi.com/?apikey=348097f5&s=code&type=movie')).resolves.toBeTruthy();
   });
+});
