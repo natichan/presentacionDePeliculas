a8bcf1320c19ad741cc13f29158078a842fcb922
Natalia <nataliamh.88@gmail.com>
2018-10-09 11:41:28 -0300

    eslitn

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "env": {
         "browser": true,
         "node": true,
-        "jest": true
+        "jest": true,
+        "commonjs": true
       },
 };
---
+++ build.config.js
@@ -0,0 +1,7 @@
+paths: {
+    es6: "node_modules/requirejs-babel/es6",
+    babel: "node_modules/requirejs-babel/babel-4.6.6.min"
+}
+define(["es6!your-es6-module"], function (module) {
+    // ...
+  });
